21af8b5fbe3340d61b0019a703ddd1892c3f26a2
Micah Schow <micah.schow@gmail.com>
2015-09-29 08:48:21 -0600

    Form Optimized
    Added radio buttons and dropdowns to script.

--- client/js/app.js
+++ client/js/app.js
@@ -6,6 +6,36 @@ angular.module('RepsAppController', ['repsService'])
   .controller('MainCtrl', function(reps){
     var main = this;
     main.reps = [];
+    main.congressType = 'reps';
+
+    main.apis = [
+      {
+        label: 'Zip',
+        method: function (zip){
+          reps('all', 'zip', zip).then(function (data){
+            main.reps = data;
+          });
+        }
+      },
+      {
+        label: 'Name',
+        method: function (name){
+          reps(main.congressType, 'name', name).then(function (data) {
+            main.reps = data;
+          });
+        }
+      },
+      {
+        label: 'State',
+        method: function (state){
+          reps(main.congressType, 'state', state).then(function (data) {
+            main.reps = data;
+          });
+        }
+      }
+    ];
+
+    main.criteria = main.apis[0];
 
       main.searchByZip = function(zip){
         reps.allByZip(zip).then(function (data){
@@ -44,45 +74,20 @@ angular
   .factory('reps', function ($http){
     var host = 'http://dgm-representatives.herokuapp.com/'
 
-    return{
-      allByZip: function(zip) {
-        return $http
-        .get( host + 'all/by-zip/' + zip)
-        .then(function (response) {
-          return response.data;
-        });
-      },
+    function search(type, criteria, query) {
+      return $http
+      .get(host + type + '/by-' + criteria + '/' + query)
+      .then(function(response) {
+        return response.data
+      });
+    }
 
-      repsByName: function (name) {
-        return $http
-        .get(host + 'reps/by-name/' + name)
-        .then(function (response){
-          return response.data;
-        });
-      },
+    //Runs the search function putting "all" and "zip" into the first two fields. "Null" is assigned to "search.this"
+    search.allByZip = search.bind(null, 'all', 'zip');
+    search.repsByName = search.bind(null, 'reps', 'name');
+    search.repsByState = search.bind(null, 'reps', 'state');
+    search.sensByName = search.bind(null, 'sens', 'name');
+    search.sensByState = search.bind(null, 'sens', 'state');
 
-      repsByState: function (state){
-        return $http
-        .get(host + 'reps/by-state/' + state)
-        .then(function (response){
-          return response.data;
-        });
-      },
-
-      sensByName: function (name){
-        return $http
-        .get(host + 'sens/by-name/' + name)
-        .then(function (response){
-          return response.data;
-        });
-      },
-
-      sensByState: function (state){
-        return $http
-        .get(host + 'sens/by-state/' + state)
-        .then(function (response){
-          return response.data;
-        });
-      },
-    };
-  });
+return search;
+});
