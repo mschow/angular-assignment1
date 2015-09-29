8e4ab619388f39769ef8071a1ebbbf8521450d34
Micah Schow <micah.schow@gmail.com>
2015-09-29 09:37:28 -0600

    Cleaned up code, added 'nicities'
    Removed superfluous HTML. Added some extra functionality (button
    disables when field is blank, loading message when searching API, etc.)

--- client/js/app.js
+++ client/js/app.js
@@ -7,29 +7,36 @@ angular.module('RepsAppController', ['repsService'])
     var main = this;
     main.reps = [];
     main.congressType = 'reps';
+    main.loading = false;
 
     main.apis = [
       {
         label: 'Zip',
         method: function (zip){
+          main.loading = true;
           reps('all', 'zip', zip).then(function (data){
             main.reps = data;
+            main.loading = false;
           });
         }
       },
       {
         label: 'Name',
         method: function (name){
+          main.loading = true;
           reps(main.congressType, 'name', name).then(function (data) {
             main.reps = data;
+            main.loading = false;
           });
         }
       },
       {
         label: 'State',
         method: function (state){
+          main.loading = true;
           reps(main.congressType, 'state', state).then(function (data) {
             main.reps = data;
+            main.loading = false;
           });
         }
       }
@@ -82,12 +89,14 @@ angular
       });
     }
 
+    search.loading = false;
+
     //Runs the search function putting "all" and "zip" into the first two fields. "Null" is assigned to "search.this"
-    search.allByZip = search.bind(null, 'all', 'zip');
+    /* search.allByZip = search.bind(null, 'all', 'zip');
     search.repsByName = search.bind(null, 'reps', 'name');
     search.repsByState = search.bind(null, 'reps', 'state');
     search.sensByName = search.bind(null, 'sens', 'name');
-    search.sensByState = search.bind(null, 'sens', 'state');
+    search.sensByState = search.bind(null, 'sens', 'state'); */
 
 return search;
 });
